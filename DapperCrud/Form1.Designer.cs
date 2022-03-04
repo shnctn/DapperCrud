73bd0585a2a01ea5b30f1cc4d27ab395b1d1ac71
Şahin ÇETİN <shnctn1@hotmail.com>
2022-03-04 23:20:20 +0300

    Test Dapper

---
+++ DapperCrud/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace DapperCrud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.btnKayit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGün = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(489, 195);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(121, 252);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(308, 20);
+            this.txtid.TabIndex = 2;
+            // 
+            // btnKayit
+            // 
+            this.btnKayit.Location = new System.Drawing.Point(130, 330);
+            this.btnKayit.Name = "btnKayit";
+            this.btnKayit.Size = new System.Drawing.Size(54, 29);
+            this.btnKayit.TabIndex = 3;
+            this.btnKayit.Text = "kaydet";
+            this.btnKayit.UseVisualStyleBackColor = true;
+            this.btnKayit.Click += new System.EventHandler(this.btnKayit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(121, 278);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(308, 20);
+            this.txtad.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 304);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Soyad";
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(121, 304);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(308, 20);
+            this.txtsoyad.TabIndex = 2;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(234, 330);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(54, 29);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGün
+            // 
+            this.btnGün.Location = new System.Drawing.Point(359, 330);
+            this.btnGün.Name = "btnGün";
+            this.btnGün.Size = new System.Drawing.Size(70, 29);
+            this.btnGün.TabIndex = 3;
+            this.btnGün.Text = "güncelle";
+            this.btnGün.UseVisualStyleBackColor = true;
+            this.btnGün.Click += new System.EventHandler(this.btnGün_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 393);
+            this.Controls.Add(this.btnGün);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKayit);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VapperVrud";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Button btnKayit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGün;
+    }
+}
+
---
+++ DapperCrud/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dapper;
+
+namespace DapperCrud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection con =new SqlConnection(ConfigurationManager.ConnectionStrings["Con"].ConnectionString);
+        private string sql = "";
+
+        void CUD(DynamicParameters dynamicParameters=null)
+        {
+
+            if (con.State==ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            con.Execute(sql,dynamicParameters,commandType:CommandType.Text);
+            con.Close();
+            foreach (Control item in this.Controls)
+            {
+                if (item is TextBox)
+                {
+                    item.Text= "";
+                }
+            }
+            dataGridView1.DataSource = con.Query<Test>("Select * from dbo.Test");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = con.Query<Test>("Select * from dbo.Test");
+
+
+        }
+
+        private void btnKayit_Click(object sender, EventArgs e)
+        {
+            DynamicParameters prm = new DynamicParameters();
+           
+            prm.Add("@p1",txtid.Text);
+            prm.Add("@p2",txtad.Text);
+            prm.Add("@p3",txtsoyad.Text);
+            sql = "insert into Test values(@p1,@p2,@p3) ";
+            CUD(prm);
+        }
+
+        private void btnGün_Click(object sender, EventArgs e)
+        {
+            DynamicParameters prm = new DynamicParameters();
+
+            prm.Add("@p1", int.Parse(txtid.Text));
+            prm.Add("@p2", txtad.Text);
+            prm.Add("@p3", txtsoyad.Text);
+            sql = "update dbo.Test set soyad=@p3, Ad=@p2 where Id =@p1";
+            CUD(prm);
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            sql="delete from test where Id='"+int.Parse(txtid.Text)+"'";
+            CUD();
+        }
+    }
+}
